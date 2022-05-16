e20252f300adb90d649a9182512894f772e4aa0c
Igor Smelyanets <ismelyanets@yahoo.com>
2022-05-16 21:09:29 +0300

    resolve task 01 HW-07

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+galleryEl.addEventListener("click", onGetLargeImage)
+
+
+const galleryMarkup = createGalleryItems(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+function onGetLargeImage(e) {
+    e.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+    instance.show();
+    document.addEventListener("keydown", e => {
+        if (e.key === "Escape") {
+            instance.close()
+        }
+    });
+    
+}
+
+
+
+
